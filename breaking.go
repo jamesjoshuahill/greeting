af7fdddddcd8184ae70cd4d4ef7f43bfb8a7ea86
Josh Hill <jhill@pivotal.io>
2019-01-29 17:59:55 +0000

    Make field public is not a breaking change

--- breaking.go
+++ breaking.go
@@ -1,32 +1,41 @@
 package greeting
 
 //////////////////////////////////////////////////
-// 1. Public field
+// 1. Constructor field param
 //
 type Greeter1 struct {
-	Greeting string
+	greeting string
 }
 
-func New1() Greeter1 {
+func New1(greeting string) Greeter1 {
+	if greeting == "" {
+		greeting = "Hello world!"
+	}
+
 	return Greeter1{
-		Greeting: "Hello world!",
+		greeting: greeting,
 	}
 }
 
 func (g Greeter1) Greet() string {
-	return g.Greeting
+	return g.greeting
 }
 
 //////////////////////////////////////////////////
-// 2. Constructor field param
+// 2. Constructor config struct param
 //
 type Greeter2 struct {
 	greeting string
 }
 
-func New2(greeting string) Greeter2 {
-	if greeting == "" {
-		greeting = "Hello world!"
+type Config struct {
+	Greeting string
+}
+
+func New2(c Config) Greeter2 {
+	greeting := "Hello world!"
+	if c.Greeting != "" {
+		greeting = c.Greeting
 	}
 
 	return Greeter2{
@@ -35,31 +44,5 @@ func New2(greeting string) Greeter2 {
 }
 
 func (g Greeter2) Greet() string {
-	return g.greeting
-}
-
-//////////////////////////////////////////////////
-// 3. Constructor config struct param
-//
-type Greeter3 struct {
-	greeting string
-}
-
-type Config struct {
-	Greeting string
-}
-
-func New3(c Config) Greeter3 {
-	greeting := "Hello world!"
-	if c.Greeting != "" {
-		greeting = c.Greeting
-	}
-
-	return Greeter3{
-		greeting: greeting,
-	}
-}
-
-func (g Greeter3) Greet() string {
 	return g.greeting
 }
--- breaking_test.go
+++ breaking_test.go
@@ -6,44 +6,29 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("Greeter1 - public field", func() {
+var _ = Describe("1. Constructor field param", func() {
 	It("greets everyone", func() {
-		greeter := greeting.New1()
+		greeter := greeting.New1("")
 
 		Expect(greeter.Greet()).To(Equal("Hello world!"))
 	})
 
 	It("has a custom greeting", func() {
-		greeter := greeting.New1()
-		greeter.Greeting = "Welcome!"
+		greeter := greeting.New1("Welcome!")
 
 		Expect(greeter.Greet()).To(Equal("Welcome!"))
 	})
 })
 
-var _ = Describe("Greeter2 - constructor field param", func() {
+var _ = Describe("2. Constructor config struct param", func() {
 	It("greets everyone", func() {
-		greeter := greeting.New2("")
+		greeter := greeting.New2(greeting.Config{})
 
 		Expect(greeter.Greet()).To(Equal("Hello world!"))
 	})
 
 	It("has a custom greeting", func() {
-		greeter := greeting.New2("Welcome!")
-
-		Expect(greeter.Greet()).To(Equal("Welcome!"))
-	})
-})
-
-var _ = Describe("Greeter3 - constructor config struct param", func() {
-	It("greets everyone", func() {
-		greeter := greeting.New3(greeting.Config{})
-
-		Expect(greeter.Greet()).To(Equal("Hello world!"))
-	})
-
-	It("has a custom greeting", func() {
-		greeter := greeting.New3(greeting.Config{Greeting: "Welcome!"})
+		greeter := greeting.New2(greeting.Config{Greeting: "Welcome!"})
 
 		Expect(greeter.Greet()).To(Equal("Welcome!"))
 	})
